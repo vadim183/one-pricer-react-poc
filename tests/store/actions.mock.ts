2042db2257fa8d7d482ec1726d361cf4a546ceb0
Vadim Siomin <vadims1987@gmail.com>
2019-01-29 15:00:04 +0200

    Delete actions.mock.ts

--- tests/store/actions.mock.ts
+++
@@ -1,8 +0,0 @@
-import { Action } from 'redux';
-import { of } from 'rxjs/index';
-
-import { ActionsObservable } from 'redux-observable';
-
-export function setupActionsMock<T extends Action>(action: T): ActionsObservable<T> {
-    return of<T>(action) as ActionsObservable<T>;
-};
