36ab1b62cf1ee716bc3ef539e9b196bbb4919aa4
Vadim Siomin <vadims1987@gmail.com>
2019-01-29 14:59:53 +0200

    Delete state.mock.ts

--- tests/store/state.mock.ts
+++
@@ -1,8 +0,0 @@
-import { of } from 'rxjs/index';
-import { StateObservable } from 'redux-observable';
-
-import { StoreState } from '@store/store-state.models';
-
-export function setupStateMock(): StateObservable<StoreState> {
-    return of<StoreState>(null) as StateObservable<StoreState>;
-}
